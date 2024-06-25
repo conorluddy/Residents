9aa2f7488233f0f70135edc03aba21ca83bdbfb9
Conor Luddy <conorluddy@gmail.com>
2024-06-25 19:31:49 +0100

    Add generic rank to users, remove redundant part of column names.

--- src/db/schema/Users.ts
+++ src/db/schema/Users.ts
@@ -21,15 +21,15 @@ export const enumUserStatus = pgEnum("userStatus", [
 
 export const tableUsers = pgTable("users", {
   id: serial("id").primaryKey(),
+  username: text("username").unique().notNull(),
   firstName: text("first_name"),
   lastName: text("last_name"),
   email: text("email").unique().notNull(),
-  userStatus: enumUserStatus("status").default("unverified"),
-  username: text("username").unique().notNull(),
-  password: text("password"),
-  createdAt: timestamp("created_at").default(sql`now()`),
   role: enumUserRole("role").default("default"),
   rank: real("rank").default(1.0),
+  password: text("password"),
+  userStatus: enumUserStatus("status").default("unverified"),
+  createdAt: timestamp("created_at").default(sql`now()`),
 })
 
 export const tableFederatedCredentials = pgTable("federatedCredentials", {
