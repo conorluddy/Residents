a496e49698dbf68bfde6e2b6f92b3b325ec42f03
Conor Luddy <conorluddy@gmail.com>
2024-07-06 20:37:16 +0100

    Tests for validateToken MW

--- src/middleware/validateRequestToken.test.ts
+++ src/middleware/validateRequestToken.test.ts
@@ -1,39 +1,58 @@
-import validateRequestToken from "./validateRequestToken"
 import { Request, Response, NextFunction } from "express"
-import { HTTP_CLIENT_ERROR } from "../constants/http"
-import { isCuid } from "@paralleldrive/cuid2"
+import validateRequestToken from "../middleware/validateRequestToken"
+import { createId } from "@paralleldrive/cuid2"
 
 describe("validateRequestToken middleware", () => {
-  let req: Partial<Request>
-  let res: Partial<Response>
-  let next: NextFunction
-  let statusMock: jest.Mock
-  let sendMock: jest.Mock
+  let mockRequest: Partial<Request>
+  let mockResponse: Partial<Response>
+  let nextFunction: NextFunction
 
   beforeEach(() => {
-    sendMock = jest.fn()
-    statusMock = jest.fn(() => ({ send: sendMock }))
-    req = {
-      headers: {},
-      // params: { token: "" },
-      // body: { token: "" },
-      // query: { token: "" },
-    }
-    res = {
-      status: statusMock,
-    }
-    next = jest.fn()
+    mockRequest = {
+      body: {
+        token: null,
+      },
+    } as Request
+
+    mockResponse = {
+      json: jest.fn(),
+      send: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    } as Partial<Response>
+
+    nextFunction = jest.fn()
   })
 
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  it("should return 400 if no token is provided", () => {
-    validateRequestToken(req as Request, res as Response, next)
+  it("should return 400 if the request token is missing", () => {
+    const expectedResponse = "A token is required"
 
-    expect(statusMock).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
-    expect(sendMock).toHaveBeenCalledWith("Unauthorized: No token provided")
-    expect(next).not.toHaveBeenCalled()
+    validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
+
+    expect(mockResponse.status).toHaveBeenCalledWith(400)
+    expect(mockResponse.send).toHaveBeenCalledWith(expectedResponse)
+  })
+
+  it("should return 400 if the request token is invalid", () => {
+    mockRequest.body.token = "invalid_token"
+    const expectedResponse = "Invalid token provided"
+
+    validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
+
+    expect(mockResponse.status).toHaveBeenCalledWith(400)
+    expect(mockResponse.send).toHaveBeenCalledWith(expectedResponse)
+  })
+
+  it("should call next function if the request token is valid", () => {
+    mockRequest.body.token = createId()
+
+    validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
+
+    expect(nextFunction).toHaveBeenCalled()
+    expect(mockResponse.status).not.toHaveBeenCalled()
+    expect(mockResponse.send).not.toHaveBeenCalled()
   })
 })
--- src/middleware/validateRequestToken.ts
+++ src/middleware/validateRequestToken.ts
@@ -1,12 +1,11 @@
 import { NextFunction, Request, RequestHandler, Response } from "express"
-
 import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR } from "../constants/http"
 import { logger } from "../utils/logger"
 import { isCuid } from "@paralleldrive/cuid2"
 
 const validateRequestToken: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.params.token || req.body.token || req.query.token
+    const token = req.params?.token || req.body?.token || req.query?.token || null
 
     if (!token) {
       return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).send("A token is required")
@@ -19,7 +18,7 @@ const validateRequestToken: RequestHandler = async (req: Request, res: Response,
     req.validatedToken = token
     next()
   } catch (error) {
-    logger.error("Token was invalid", error)
+    logger.error("Token validation threw error: ", error)
     return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).send("Error validating token")
   }
 }
