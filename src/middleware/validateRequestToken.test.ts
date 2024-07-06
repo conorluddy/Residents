4c1a59a559ff03396c76e3d34246301afe8dc738
Conor Luddy <conorluddy@gmail.com>
2024-07-06 20:50:08 +0100

    Tests for email validation middleware

--- src/middleware/validateRequestToken.test.ts
+++ src/middleware/validateRequestToken.test.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import validateRequestToken from "../middleware/validateRequestToken"
 import { createId } from "@paralleldrive/cuid2"
 
-describe("validateRequestToken middleware", () => {
+describe("Middleware: validateRequestToken", () => {
   let mockRequest: Partial<Request>
   let mockResponse: Partial<Response>
   let nextFunction: NextFunction
@@ -13,13 +13,10 @@ describe("validateRequestToken middleware", () => {
         token: null,
       },
     } as Request
-
     mockResponse = {
-      json: jest.fn(),
-      send: jest.fn(),
+      send: jest.fn().mockReturnThis(),
       status: jest.fn().mockReturnThis(),
     } as Partial<Response>
-
     nextFunction = jest.fn()
   })
 
@@ -28,29 +25,21 @@ describe("validateRequestToken middleware", () => {
   })
 
   it("should return 400 if the request token is missing", () => {
-    const expectedResponse = "A token is required"
-
     validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
-
     expect(mockResponse.status).toHaveBeenCalledWith(400)
-    expect(mockResponse.send).toHaveBeenCalledWith(expectedResponse)
+    expect(mockResponse.send).toHaveBeenCalledWith("A token is required")
   })
 
   it("should return 400 if the request token is invalid", () => {
     mockRequest.body.token = "invalid_token"
-    const expectedResponse = "Invalid token provided"
-
     validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
-
     expect(mockResponse.status).toHaveBeenCalledWith(400)
-    expect(mockResponse.send).toHaveBeenCalledWith(expectedResponse)
+    expect(mockResponse.send).toHaveBeenCalledWith("Invalid token provided")
   })
 
   it("should call next function if the request token is valid", () => {
     mockRequest.body.token = createId()
-
     validateRequestToken(mockRequest as Request, mockResponse as Response, nextFunction)
-
     expect(nextFunction).toHaveBeenCalled()
     expect(mockResponse.status).not.toHaveBeenCalled()
     expect(mockResponse.send).not.toHaveBeenCalled()
