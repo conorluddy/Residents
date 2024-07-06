4d69205823d5381072cc881977e023be219e8f44
Conor Luddy <conorluddy@gmail.com>
2024-07-06 10:27:46 +0100

    update-postman-and-tweak-routes

--- src/routes/auth/validateAccount.ts
+++ src/routes/auth/validateAccount.ts
@@ -4,6 +4,6 @@ import CONTROLLERS from "../../controllers"
 
 const router = Router()
 
-router.get("/validate/:token", authenticateToken, CONTROLLERS.AUTH.validateAccount)
+router.patch("/validate/:token/:userId", authenticateToken, CONTROLLERS.AUTH.validateAccount)
 
 export default router
