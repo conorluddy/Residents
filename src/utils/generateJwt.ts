281dba1c04cd6c6dfb9ee53ed4962313d764d125
Conor Luddy <conorluddy@gmail.com>
2024-07-07 16:26:01 +0100

    JWT middleware 100% cov

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -4,12 +4,12 @@ import { UserNoPW } from "../db/types"
 
 const DEFAULT_JWT_TOKEN_EXPIRY = "1m"
 
-export const generateJwt = (userPayload: JWTUserPayload) => {
+export const generateJwt = (userPayload: JWTUserPayload, expiryOverride?: string) => {
   const secret = process.env.JWT_TOKEN_SECRET
   if (secret == null) throw new Error("JWT secret not found")
 
   return jwt.sign(userPayload, secret, {
-    expiresIn: process.env.JWT_TOKEN_EXPIRY ?? DEFAULT_JWT_TOKEN_EXPIRY,
+    expiresIn: expiryOverride ?? process.env.JWT_TOKEN_EXPIRY ?? DEFAULT_JWT_TOKEN_EXPIRY,
   })
 }
 
