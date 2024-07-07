0e93a2362fa9aaa28ceec6060636ce7279e5a250
Conor Luddy <conorluddy@gmail.com>
2024-07-07 12:34:36 +0100

    RBAC tests: User role superiority

--- src/middleware/roleBasedAccessControl.ts
+++ src/middleware/roleBasedAccessControl.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from "express"
 import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR } from "../constants/http"
 import { JWTUserPayload } from "../utils/generateJwt"
 import { ACL, PERMISSIONS } from "../constants/accessControlList"
-import db from "../db"
 import { tableUsers } from "../db/schema"
 import { eq } from "drizzle-orm"
 import { logger } from "../utils/logger"
 import { ROLES, ROLES_ARRAY } from "../constants/database"
+import db from "../db"
 
 export const RBAC = {
   canGetOwnUser: checkPermission(PERMISSIONS.CAN_GET_OWN_USER),
@@ -17,7 +17,7 @@ export const RBAC = {
   checkCanDeleteUser: checkPermission(PERMISSIONS.CAN_DELETE_ANY_USER),
   checkCanUpdateAnyUserStatus: checkPermission(PERMISSIONS.CAN_UPDATE_ANY_USER_STATUS),
   checkCanUpdateOwnProfile: checkPermission(PERMISSIONS.CAN_UPDATE_OWN_USER),
-  checkRoleSuperiority: checkRoleSuperiority(),
+  checkRoleSuperiority,
 }
 
 /**
@@ -46,54 +46,59 @@ function checkPermission(permission: PERMISSIONS) {
 /**
  * Check if the user has role superiority over the target user
  */
-function checkRoleSuperiority() {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as JWTUserPayload
-    const targetUserId = req.params.id
+async function checkRoleSuperiority(req: Request, res: Response, next: NextFunction) {
+  const user = req.user as JWTUserPayload
+  const targetUserId = req.params.id
 
-    try {
-      if (!user.role) {
-        logger.warn(`User ${user.id} is missing a role`)
-        return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "User has no role" })
-      }
-
-      const targetUser = await db.select().from(tableUsers).where(eq(tableUsers.id, targetUserId))
-
-      // No user found
-      if (!targetUser[0]?.role) {
-        logger.error(`Target user ${targetUserId} not found`)
-        return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "Target not found" })
-      }
-
-      // Target user is locked and can only be edited by Admin or Owner. (Improve this later / make configurable)
-      if (targetUser[0]?.role === ROLES.LOCKED && ![ROLES.ADMIN, ROLES.OWNER].includes(user.role)) {
-        logger.warn(`User ${targetUserId} is locked`)
-        return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Target is locked" })
-      }
-
-      // Get the Role rankings
-      const userRoleIndex = ROLES_ARRAY.findIndex((role) => role === user.role)
-      const targetRoleIndex = ROLES_ARRAY.findIndex((role) => role === targetUser[0]?.role)
-
-      // Fail early if no roles found
-      if (userRoleIndex === -1 || targetRoleIndex === -1) {
-        logger.error(`User roles not found for user ${user.id} or target ${targetUserId}`)
-        return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Roles not found" })
-      }
-
-      // Check if the user has role superiority based on the role index
-      if (targetRoleIndex <= userRoleIndex) {
-        logger.warn(`User ${user.id} lacks role superiority over target ${targetUserId}`)
-        return res
-          .status(HTTP_CLIENT_ERROR.FORBIDDEN)
-          .json({ message: "Role superiority is required for this operation" })
-      }
-
-      req.targetUserId = targetUser[0].id
-      next()
-    } catch (error) {
-      logger.error(`Error checking role superiority for user ${user.id} and target ${targetUserId}: ${error}`)
-      return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error" })
+  try {
+    if (!user.role) {
+      logger.warn(`User ${user.id} is missing a role`)
+      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "User has no role" })
     }
+
+    const targetUsers = await db.select().from(tableUsers).where(eq(tableUsers.id, targetUserId))
+    const targetUser = targetUsers[0]
+
+    // No user found
+    if (!targetUser) {
+      logger.error(`Target user ${targetUserId} not found`)
+      return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "Target user not found" })
+    }
+
+    // User has no role
+    if (!targetUser.role) {
+      logger.error(`Target user ${targetUserId} has no role`)
+      return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "Target user not found" })
+    }
+
+    // Target user is locked and can only be edited by Admin or Owner. (Improve this later / make configurable)
+    if (targetUser?.role === ROLES.LOCKED && ![ROLES.ADMIN, ROLES.OWNER].includes(user.role)) {
+      logger.warn(`User ${targetUserId} is locked`)
+      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Target is locked" })
+    }
+
+    // Get the Role rankings
+    const userRoleIndex = ROLES_ARRAY.findIndex((role) => role === user.role)
+    const targetRoleIndex = ROLES_ARRAY.findIndex((role) => role === targetUser.role)
+
+    // Fail early if no roles found
+    if (userRoleIndex === -1 || targetRoleIndex === -1) {
+      logger.error(`User roles not found for user ${user.id} or target ${targetUserId}`)
+      return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Roles not found" })
+    }
+
+    // Check if the user has role superiority based on the role index
+    if (targetRoleIndex <= userRoleIndex) {
+      logger.warn(`User ${user.id} lacks role superiority over target ${targetUserId}`)
+      return res
+        .status(HTTP_CLIENT_ERROR.FORBIDDEN)
+        .json({ message: "Role superiority is required for this operation" })
+    }
+
+    req.targetUserId = targetUser.id
+    next()
+  } catch (error) {
+    logger.error(`Error checking role superiority for user ${user.id} and target ${targetUserId}: ${error}`)
+    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error" })
   }
 }
