1c655deaad5390e6f08d40f2cf50ad8bb8f83c9b
Conor Luddy <conorluddy@gmail.com>
2024-07-12 20:26:46 +0100

    Tests for discardToken MW

--- src/middleware/discardToken.ts
+++ src/middleware/discardToken.ts
@@ -4,32 +4,36 @@ import { logger } from "../utils/logger"
 import { eq } from "drizzle-orm"
 import db from "../db"
 import { tableTokens } from "../db/schema"
+import { TokenWithUser } from "../db/types"
 
 /**
  * Middleware for discarding a token after it has been used.
  */
 const discardToken: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { tokenWithUser } = req
+    const tokenWithUser: TokenWithUser = req.tokenWithUser
 
     if (!tokenWithUser) {
-      logger.error(`Missing tokenWithUser object in discardToken middleware`)
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).send("Token expired")
+      logger.error(`Missing token in discardToken middleware`)
+      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Token invalid" })
     }
 
-    const updatedToken = await db.update(tableTokens).set({ used: true }).where(eq(tableTokens.id, tokenWithUser.id))
+    const [updatedToken] = await db
+      .update(tableTokens)
+      .set({ used: true })
+      .where(eq(tableTokens.id, tokenWithUser.id))
+      .returning()
 
-    if (updatedToken.rowCount !== 1) {
-      logger.error(`Error expiring token: ${tokenWithUser.id}`)
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).send("Token expired")
+    if (!updatedToken || updatedToken?.id !== tokenWithUser?.id) {
+      logger.error(`Error expiring token ID:${tokenWithUser.id}`)
+      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Token invalid" })
     }
 
     // Probably just delete it here instead of setting it to used
-
     next()
   } catch (error) {
     logger.error(error)
-    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).send("Error")
+    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Error" })
   }
 }
 
