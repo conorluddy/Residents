2ab737334e4e399b0ef966b3921b816a391b824c
Conor Luddy <conorluddy@gmail.com>
2024-07-14 11:10:50 +0100

    More test coverage for resetPassword and updateUser controllers

--- src/controllers/auth/resetPasswordWithToken.test.ts
+++ src/controllers/auth/resetPasswordWithToken.test.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { TOKEN_TYPE } from "../../constants/database"
-import { HTTP_SUCCESS } from "../../constants/http"
+import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http"
 import { TokenWithUser } from "../../db/types"
 import { makeAFakeUser } from "../../test-utils/mockUsers"
 import { logger } from "../../utils/logger"
@@ -28,7 +28,7 @@ jest.mock("../../utils/logger", () => ({
 }))
 
 describe("Controller: Reset Password With Token", () => {
-  let mockRequest: Partial<Request> & { tokenWithUser: TokenWithUser }
+  let mockRequest: Partial<Request> & { tokenWithUser?: TokenWithUser }
   let mockResponse: Partial<Response>
   let mockNext: Partial<NextFunction>
 
@@ -58,7 +58,47 @@ describe("Controller: Reset Password With Token", () => {
   it("Happy path: Reset Password With Token", async () => {
     await resetPasswordWithToken(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
     expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SUCCESS.OK)
-    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Password successfully updated" })
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Password successfully updated." })
     expect(logger.info).toHaveBeenCalledWith("Password successfully reset for bananaman@ireland.ie")
+  })
+
+  it(`Returns forbidden when missing token`, async () => {
+    mockRequest.tokenWithUser = undefined
+    await resetPasswordWithToken(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.FORBIDDEN)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Token missing." })
+    expect(logger.error).toHaveBeenCalledWith("Missing tokenWithUser data in resetPasswordWithToken controller")
+  })
+
+  it(`Returns forbidden when an incorrect type of token is used`, async () => {
+    mockRequest.tokenWithUser = {
+      ...mockRequest.tokenWithUser!,
+      type: TOKEN_TYPE.MAGIC,
+    }
+    await resetPasswordWithToken(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.FORBIDDEN)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Invalid token type." })
+    expect(logger.error).toHaveBeenCalledWith(
+      "An incorrect token type was used in an attempt to reset a password: TID:123"
+    )
+  })
+
+  it(`Returns bad request when the requested new password fails password strength test`, async () => {
+    mockRequest.body.password = "password1"
+    await resetPasswordWithToken(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Password not strong enough, try harder." })
+    expect(logger.error).toHaveBeenCalledWith("Password reset attempt failed with weak password.")
+  })
+
+  it(`Returns a server error when IDs dont match.`, async () => {
+    mockRequest.tokenWithUser = {
+      ...mockRequest.tokenWithUser!,
+      user: makeAFakeUser({ id: "DIFFERENT_ID" }),
+      user_id: "DIFFERENT_ID",
+    }
+    await resetPasswordWithToken(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Error updating password." })
   })
 })
--- src/controllers/users/updateUser.test.ts
+++ src/controllers/users/updateUser.test.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { makeAFakeUser } from "../../test-utils/mockUsers"
-import { HTTP_SUCCESS } from "../../constants/http"
+import { HTTP_CLIENT_ERROR, HTTP_SUCCESS } from "../../constants/http"
 import { updateUser } from "./updateUser"
 import { User } from "../../db/types"
 
@@ -21,7 +21,7 @@ jest.mock("../../db", () => ({
 }))
 
 describe("Controller: UpdateUser", () => {
-  let mockRequest: Partial<Request> & { targetUserId: string }
+  let mockRequest: Partial<Request> & { params: { id?: string }; targetUserId?: string }
   let mockResponse: Partial<Response>
   beforeAll(() => {})
   beforeEach(() => {
@@ -43,9 +43,48 @@ describe("Controller: UpdateUser", () => {
     }
   })
 
-  it("Update User - Happy path", async () => {
+  it("Successfully updates a user", async () => {
     await updateUser(mockRequest as Request, mockResponse as Response)
     expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SUCCESS.OK)
     expect(mockResponse.json).toHaveBeenCalledWith({ message: `User ${fakeUser.id} updated successfully` })
+  })
+
+  it("Responds with Bad Request when IDs are missing", async () => {
+    mockRequest.params = {
+      ...mockRequest.params!,
+      id: "",
+    }
+    await updateUser(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: `User ID is missing in the request.` })
+  })
+
+  it("Responds with Forbidden if ID and verified target ID dont match", async () => {
+    mockRequest.params = {
+      ...mockRequest.params!,
+      id: "NotTheFakerUsersID",
+    }
+    await updateUser(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.FORBIDDEN)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: `You are not allowed to update this user.` })
+  })
+
+  it("Responds with Bad Request if no update data is provided", async () => {
+    mockRequest.body = {}
+    await updateUser(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: `No data provided to update the user with.` })
+  })
+
+  it("Responds with Bad Request if no update data is provided", async () => {
+    mockRequest.body = {
+      ...mockRequest.body,
+      email: "bademail",
+    }
+    await updateUser(mockRequest as Request, mockResponse as Response)
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
+    expect(mockResponse.json).toHaveBeenCalledWith({
+      message: `You need to use a valid email address if you want to update an email. bademail is not valid.`,
+    })
   })
 })
