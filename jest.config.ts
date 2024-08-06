4df40ade2d6ab070aed26d0f8da73ae75f497b51
Conor Luddy <conorluddy@gmail.com>
2024-08-06 20:11:43 +0100

    Ignore coverage for some boilerplatey stuff

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,18 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!dist/**/*"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!dist/**/*",
+    "!src/config.ts",
+    // This is really all boilerplate for Drizzle
+    "!src/db/index.ts",
+    // Might want to include these in the future but should be okay for now
+    "!src/passport/**/*",
+    // Just the logger setup
+    "!src/utils/logger.ts",
+  ],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   setupFiles: ["<rootDir>/tests/jest.setup.ts"],
 }
