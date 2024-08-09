a6d43dbdad6a287e19af345d832be4f5f2894978
Conor <conorluddy@gmail.com>
2024-08-09 22:13:27 +0100

    Testing: integration-testing-setup (#81)

---
+++ tests/integration/happyPath.test.ts
@@ -0,0 +1,46 @@
+import request from "supertest"
+import { app } from "../../src"
+import { dbClient } from "../../src/db"
+
+describe("Integration: Test flow", () => {
+  let jwt: string
+
+  beforeAll(async () => {
+    await dbClient.connect()
+    await dbClient.query("DELETE FROM users where username = 'mrhappy'")
+  })
+
+  afterAll(async () => await dbClient.end())
+
+  it("Create a new user", async () => {
+    const newUser = {
+      firstName: "mrhappy",
+      lastName: "mrhappy",
+      email: "mrhappy@resi.dents",
+      username: "mrhappy",
+      password: "STRONGP4$$w0rd_",
+    }
+    const response = await request(app).post("/users/register").send(newUser)
+    expect(response.status).toBe(201)
+    expect(response.body).toHaveProperty("message", "User registered.")
+  })
+
+  it("Tries to hit a private endpoint without a token and gets bounced", async () => {
+    const response = await request(app).get("/users/self")
+    expect(response.status).toBe(401)
+    expect(response.body).toMatchObject({
+      message: "Token is required",
+    })
+  })
+
+  it("Log in with the new user", async () => {
+    const login = {
+      username: "mrhappy",
+      password: "STRONGP4$$w0rd_",
+    }
+    const response = await request(app).post("/auth").send(login)
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("accessToken")
+    jwt = response.body.accessToken
+  })
+})
