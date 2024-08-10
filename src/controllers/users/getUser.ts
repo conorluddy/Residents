6d9180dbfe358336e8f6cef1ebfa4484f131d623
Conor <conorluddy@gmail.com>
2024-08-10 11:20:11 +0100

    Couple of extra tests for getUser(s) (#83)

--- src/controllers/users/getUser.ts
+++ src/controllers/users/getUser.ts
@@ -17,17 +17,16 @@ export const getUser = async (req: Request, res: Response) => {
     }
 
     // Add a WITHMETA flag to include the user meta data
-
     const user = await db.select().from(tableUsers).where(eq(tableUsers.id, userId))
 
     if (!user) {
-      return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "User not found" })
+      return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "User not found." })
     }
 
     return res.status(HTTP_SUCCESS.OK).json(user)
   } catch (error) {
     logger.error(error)
-    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Error getting user" })
+    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Error getting user." })
   }
 }
 
