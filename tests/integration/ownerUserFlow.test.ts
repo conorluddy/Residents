e7520d00da00cb63e25f674d5456844195a41353
Conor Luddy <conorluddy@gmail.com>
2024-08-10 11:16:04 +0100

    Unskip some of the owner integration tests

--- tests/integration/ownerUserFlow.test.ts
+++ tests/integration/ownerUserFlow.test.ts
@@ -13,7 +13,7 @@ import { seedUsers } from "../../src/db/utils/seedUsers"
  * - Get a specific user
  */
 
-describe.skip("Integration: Owner flow from seeded default owner", () => {
+describe("Integration: Owner flow from seeded default owner", () => {
   let jwt: string
 
   beforeAll(async () => {
@@ -49,13 +49,13 @@ describe.skip("Integration: Owner flow from seeded default owner", () => {
     expect(response.status).toBe(200)
   })
 
-  it("Hit the /getAllUsers endpoint once logged in and get all of the users back", async () => {
+  it.skip("Hit the /getAllUsers endpoint once logged in and get all of the users back", async () => {
     const response = await request(app).get("/users").set("Authorization", `Bearer ${jwt}`)
     expect(response.status).toBe(200)
     expect(response.body).toHaveLength(61) // 10 seeded users + the owner
   })
 
-  it("Call getAllUsers again and delete one of them", async () => {
+  it.skip("Call getAllUsers again and delete one of them", async () => {
     const usersResponse = await request(app).get("/users").set("Authorization", `Bearer ${jwt}`)
     const userIdToDelete = usersResponse.body[3]?.id
     const deleteResponse = await request(app)
@@ -65,7 +65,7 @@ describe.skip("Integration: Owner flow from seeded default owner", () => {
     expect(deleteResponse.status).toBe(200)
   })
 
-  it("Call getAllUsers again and then get a specific one", async () => {
+  it.skip("Call getAllUsers again and then get a specific one", async () => {
     const usersResponse = await request(app).get("/users").set("Authorization", `Bearer ${jwt}`)
     const userIdToGet = usersResponse.body[5]?.id
     const userResponse = await request(app)
