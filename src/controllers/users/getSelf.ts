6d19170b362eec112dad76b852895fd4841083e4
Conor Luddy <conorluddy@gmail.com>
2024-08-31 19:39:17 +0100

    GetUser

--- src/controllers/users/getSelf.ts
+++ src/controllers/users/getSelf.ts
@@ -8,14 +8,13 @@ import { BadRequestError, NotFoundError } from "../../errors"
  * getSelf - gets own user record
  */
 export const getSelf = async (req: Request, res: Response, next: NextFunction) => {
+  //
   const userId = req[REQUEST_USER]?.id
-
   if (!userId) throw new BadRequestError("User ID is missing.")
-
+  //
   const user = await SERVICES.getUserByID(userId)
-
   if (!user) throw new NotFoundError("User not found.")
-
+  //
   return res.status(HTTP_SUCCESS.OK).json(user)
 }
 
