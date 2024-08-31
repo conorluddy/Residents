0e359afa8c505911c638bb8951a7df4208f859c3
Conor Luddy <conorluddy@gmail.com>
2024-08-31 19:20:52 +0100

    GetSelf

--- src/controllers/users/getSelf.test.ts
+++ src/controllers/users/getSelf.test.ts
@@ -1,36 +1,31 @@
-import { Request, Response } from "express"
-import { makeAFakeSafeUser, makeAFakeUser } from "../../test-utils/mockUsers"
-import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http"
-import { getSelf } from "./getSelf"
+import { NextFunction, Request, Response } from "express"
+import { HTTP_SUCCESS } from "../../constants/http"
 import { SafeUser, User } from "../../db/types"
-import { logger } from "../../utils/logger"
+import { makeAFakeSafeUser, makeAFakeUser } from "../../test-utils/mockUsers"
 import { REQUEST_USER } from "../../types/requestSymbols"
+import { getSelf } from "./getSelf"
 
 let fakeUser: Partial<User>
 
-jest.mock("../../utils/logger")
-jest.mock("../../db", () => ({
-  select: jest.fn().mockReturnValue({
-    from: jest.fn().mockReturnValue({
-      where: jest
-        .fn()
-        .mockImplementationOnce(async () => {
-          fakeUser = makeAFakeUser({ id: "SELF_ID" })
-          return [fakeUser]
-        })
-        .mockImplementationOnce(async () => {
-          return []
-        })
-        .mockImplementationOnce(async () => {
-          throw new Error("DB error")
-        }),
+jest.mock("../../services/index", () => ({
+  getUserByID: jest
+    .fn()
+    .mockImplementationOnce(async () => {
+      fakeUser = makeAFakeUser({ id: "SELF_ID" })
+      return fakeUser
+    })
+    .mockImplementationOnce(async () => {
+      return undefined
+    })
+    .mockImplementationOnce(async () => {
+      throw new Error("DB error")
     }),
-  }),
 }))
 
 describe("Controller: GetSelf", () => {
   let mockRequest: Partial<Request> & { [REQUEST_USER]?: SafeUser }
   let mockResponse: Partial<Response>
+  let mockNext = jest.fn().mockReturnThis()
 
   beforeEach(() => {
     mockRequest = {
@@ -43,28 +38,21 @@ describe("Controller: GetSelf", () => {
   })
 
   it("Happy path", async () => {
-    await getSelf(mockRequest as Request, mockResponse as Response)
+    await getSelf(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)
     expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SUCCESS.OK)
     expect(mockResponse.json).toHaveBeenCalledWith(fakeUser)
   })
 
   it("Missing ID is handled", async () => {
     mockRequest[REQUEST_USER] = undefined
-    await getSelf(mockRequest as Request, mockResponse as Response)
-    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.BAD_REQUEST)
-    expect(mockResponse.json).toHaveBeenCalledWith({ message: "User ID is missing in the request." })
+    await expect(getSelf(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)).rejects.toThrow(
+      "User ID is missing."
+    )
   })
 
   it("User not found in DB (shouldnt happen for getSelf but o/)", async () => {
-    await getSelf(mockRequest as Request, mockResponse as Response)
-    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_CLIENT_ERROR.NOT_FOUND)
-    expect(mockResponse.json).toHaveBeenCalledWith({ message: "User not found." })
-  })
-
-  it("Unhappy path", async () => {
-    await getSelf(mockRequest as Request, mockResponse as Response)
-    expect(logger.error).toHaveBeenCalledWith("Error getting user by ID", Error("DB error"))
-    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR)
-    expect(mockResponse.json).toHaveBeenCalledWith({ message: "Internal server error while getting user." })
+    await expect(getSelf(mockRequest as Request, mockResponse as Response, mockNext as NextFunction)).rejects.toThrow(
+      "User not found."
+    )
   })
 })
--- src/controllers/users/getSelf.ts
+++ src/controllers/users/getSelf.ts
@@ -1,30 +1,22 @@
-import { Request, Response } from "express"
-import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http"
+import { NextFunction, Request, Response } from "express"
+import { HTTP_SUCCESS } from "../../constants/http"
 import { REQUEST_USER } from "../../types/requestSymbols"
-import { logger } from "../../utils/logger"
 import SERVICES from "../../services"
+import { BadRequestError, NotFoundError } from "../../errors"
 
 /**
  * getSelf - gets own user record
  */
-export const getSelf = async (req: Request, res: Response) => {
-  try {
-    const userId = req[REQUEST_USER]?.id
+export const getSelf = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req[REQUEST_USER]?.id
 
-    if (!userId)
-      return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).json({ message: "User ID is missing in the request." })
+  if (!userId) throw new BadRequestError("User ID is missing.")
 
-    const user = await SERVICES.getUserByID(userId)
+  const user = await SERVICES.getUserByID(userId)
 
-    if (!user) return res.status(HTTP_CLIENT_ERROR.NOT_FOUND).json({ message: "User not found." })
+  if (!user) throw new NotFoundError("User not found.")
 
-    return res.status(HTTP_SUCCESS.OK).json(user)
-  } catch (error) {
-    logger.error(error)
-    return res
-      .status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error while getting user." })
-  }
+  return res.status(HTTP_SUCCESS.OK).json(user)
 }
 
 export default getSelf
