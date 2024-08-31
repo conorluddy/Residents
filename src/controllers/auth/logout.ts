5f351bed24aabd29e949b65d2814f946a7c275f2
Conor Luddy <conorluddy@gmail.com>
2024-08-31 20:24:42 +0100

    Logout

--- src/controllers/auth/logout.ts
+++ src/controllers/auth/logout.ts
@@ -1,42 +1,33 @@
-import { Request, Response } from "express"
-import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http"
+import { NextFunction, Request, Response } from "express"
+import { HTTP_SUCCESS } from "../../constants/http"
+import { BadRequestError } from "../../errors"
 import SERVICES from "../../services"
 import { REQUEST_USER } from "../../types/requestSymbols"
-import { logger } from "../../utils/logger"
 
 /**
  * logout
  */
-export const logout = async (req: Request, res: Response) => {
-  try {
-    const userId = req[REQUEST_USER]?.id
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req[REQUEST_USER]?.id
+  if (!userId) throw new BadRequestError("User ID is missing.")
 
-    if (!userId) {
-      logger.error("User ID not found", { userId })
-      return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).json({ message: "User ID not found" })
-    }
+  // TODO: Clear the refreshToken and xsrfToken cookies
 
-    // TODO: Clear the refreshToken and xsrfToken cookies
+  res.cookie("refreshToken", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    expires: new Date(),
+  })
 
-    res.cookie("refreshToken", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      expires: new Date(),
-    })
+  res.cookie("xsrfToken", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    expires: new Date(),
+  })
 
-    res.cookie("xsrfToken", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      expires: new Date(),
-    })
+  await SERVICES.deleteRefreshToken({ userId })
 
-    await SERVICES.deleteRefreshToken({ userId })
-
-    return res.status(HTTP_SUCCESS.OK).json({ message: "Logged out - Come back soon!" })
-  } catch (error) {
-    logger.error("Error logging out", { error })
-    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Error logging out" })
-  }
+  return res.status(HTTP_SUCCESS.OK).json({ message: "Logged out from API. Clear your client tokens." })
 }
