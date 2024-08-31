3955c43b3e9a3e22f522d1c993b729929ccda60c
Conor Luddy <conorluddy@gmail.com>
2024-08-31 20:13:23 +0100

    GoogleCB and Login

--- src/controllers/auth/googleCallback.ts
+++ src/controllers/auth/googleCallback.ts
@@ -1,24 +1,16 @@
-import { Request, Response } from "express"
-import { HTTP_CLIENT_ERROR, HTTP_SUCCESS } from "../../constants/http"
-import { logger } from "../../utils/logger"
+import { NextFunction, Request, Response } from "express"
+import { HTTP_SUCCESS } from "../../constants/http"
 import { REQUEST_USER } from "../../types/requestSymbols"
 import { generateJwtFromUser } from "../../utils/generateJwt"
 
 /**
  * googleCallback
- * GET
  */
-export const googleCallback = async (req: Request, res: Response) => {
-  try {
-    if (!req[REQUEST_USER]) throw new Error("User not found")
-
-    // TODO: Revalidate before returning token
-    // This isn't secure, you can just fire a made up user at it in the request and get a token.
-    // Probably need to check request for google related stuff and validate that the user is who they say they are.
-    const token = generateJwtFromUser(req[REQUEST_USER])
-    return res.status(HTTP_SUCCESS.OK).json({ token })
-  } catch (error) {
-    logger.error(error)
-    return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Error logging in" })
-  }
+export const googleCallback = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req[REQUEST_USER]) throw new Error("User not found")
+  // TODO: Revalidate before returning token
+  // This isn't secure, you can just fire a made up user at it in the request and get a token.
+  // Probably need to check request for google related stuff and validate that the user is who they say they are.
+  const token = generateJwtFromUser(req[REQUEST_USER])
+  return res.status(HTTP_SUCCESS.OK).json({ token })
 }
--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,87 +1,63 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { isEmail } from "validator"
 import { TOKEN_TYPE } from "../../constants/database"
-import { HTTP_CLIENT_ERROR, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../constants/http"
+import { HTTP_SUCCESS } from "../../constants/http"
 import { TIMESPAN } from "../../constants/time"
 import { User } from "../../db/schema"
+import { BadRequestError, ForbiddenError } from "../../errors"
 import generateXsrfToken from "../../middleware/util/xsrfToken"
 import SERVICES from "../../services"
 import { validateHash } from "../../utils/crypt"
 import { generateJwtFromUser } from "../../utils/generateJwt"
-import { logger } from "../../utils/logger"
 
 /**
  * login
  */
-export const login = async (req: Request, res: Response) => {
-  try {
-    const { username, email, password } = (req.body ?? {}) as Pick<User, "username" | "email" | "password">
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { username, email, password } = (req.body ?? {}) as Pick<User, "username" | "email" | "password">
 
-    if (!username && !email) {
-      return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).json({ message: "Username or email is required" })
-    }
+  if (!username && !email) throw new BadRequestError("Username or email is required")
 
-    if (!password) {
-      return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).json({ message: "Password is required" })
-    }
+  if (!password) throw new BadRequestError("Password is required")
 
-    if (email && !isEmail(email)) {
-      return res.status(HTTP_CLIENT_ERROR.BAD_REQUEST).json({ message: "Invalid email address" })
-    }
+  if (email && !isEmail(email)) throw new BadRequestError("Invalid email address")
 
-    const getUserByUsernameOrEmail = username ? SERVICES.getUserByUsername : SERVICES.getUserByEmail
-    const user = await getUserByUsernameOrEmail(username ?? email)
+  const getUserByUsernameOrEmail = username ? SERVICES.getUserByUsername : SERVICES.getUserByEmail
+  const user = await getUserByUsernameOrEmail(username ?? email)
 
-    if (!user) {
-      // Should we throw here and then clear auth in the catch and res from there??
-      // Should probably clear any existing auth here
-      // logger.error("No user found")
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Nope." })
-    }
+  if (!user) throw new ForbiddenError("Nope.") // Should probably clear any existing auth here
 
-    const passwordHash = await SERVICES.getUserPasswordHash(user.id)
+  const passwordHash = await SERVICES.getUserPasswordHash(user.id)
 
-    if (!passwordHash) {
-      // logger.error("No password found")
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Nope." })
-    }
+  if (!passwordHash) throw new ForbiddenError("Nope.")
 
-    if (!(await validateHash(password, passwordHash))) {
-      // logger.error("Password validation failed", { password, passwordHash })
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Nope." })
-    }
+  if (!(await validateHash(password, passwordHash))) throw new ForbiddenError("Nope.")
 
-    const refreshTokenId = await SERVICES.createToken({
-      userId: user.id,
-      type: TOKEN_TYPE.REFRESH,
-      expiry: TIMESPAN.WEEK,
-    })
+  const refreshTokenId = await SERVICES.createToken({
+    userId: user.id,
+    type: TOKEN_TYPE.REFRESH,
+    expiry: TIMESPAN.WEEK,
+  })
 
-    if (!refreshTokenId) {
-      return res.status(HTTP_CLIENT_ERROR.FORBIDDEN).json({ message: "Nope." })
-    }
+  if (!refreshTokenId) throw new ForbiddenError("Nope.")
 
-    const accessToken = generateJwtFromUser(user)
-    const xsrfToken = generateXsrfToken()
+  const accessToken = generateJwtFromUser(user)
+  const xsrfToken = generateXsrfToken()
 
-    // Set the tokens in a HTTP-only secure cookies
-    res.cookie("refreshToken", refreshTokenId, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: TIMESPAN.WEEK,
-    })
+  // Set the tokens in a HTTP-only secure cookies
+  res.cookie("refreshToken", refreshTokenId, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: TIMESPAN.WEEK,
+  })
 
-    res.cookie("xsrfToken", xsrfToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: TIMESPAN.WEEK,
-    })
+  res.cookie("xsrfToken", xsrfToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: TIMESPAN.WEEK,
+  })
 
-    return res.status(HTTP_SUCCESS.OK).json({ accessToken })
-  } catch (error) {
-    logger.error(error)
-    return res.status(HTTP_SERVER_ERROR.INTERNAL_SERVER_ERROR).json({ message: "Error logging in." })
-  }
+  return res.status(HTTP_SUCCESS.OK).json({ accessToken })
 }
