41f343755c6b5d7a3a2b9e17a2e90b85067e59a1
Conor Luddy <conorluddy@gmail.com>
2024-09-03 07:52:09 +0100

    Remove the final few direct error responses

--- src/controllers/auth/magicLoginWithToken.ts
+++ src/controllers/auth/magicLoginWithToken.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express"
 import { HTTP_SERVER_ERROR } from "../../constants/http"
+import { NotImplementedError } from "../../errors"
 
 /**
  * GET: magicLoginWithToken
  */
 export const magicLoginWithToken = async (_req: Request, res: Response, next: NextFunction) => {
-  return res.status(HTTP_SERVER_ERROR.NOT_IMPLEMENTED).json({ message: "Not implemented yet." })
+  throw new NotImplementedError("Not implemented yet.")
 }
