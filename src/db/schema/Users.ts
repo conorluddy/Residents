3e34d3a8fed81ca54888906564ad327bc3b6f6e1
Conor Luddy <conorluddy@gmail.com>
2024-09-22 17:50:26 +0100

    USer indexing

--- src/db/schema/Users.ts
+++ src/db/schema/Users.ts
@@ -1,6 +1,6 @@
 import { createId } from '@paralleldrive/cuid2'
 import { relations, sql } from 'drizzle-orm'
-import { pgEnum, pgTable, pgView, text, timestamp } from 'drizzle-orm/pg-core'
+import { index, pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { ROLES, ROLES_ARRAY, STATUS, STATUS_ARRAY } from '../../constants/database'
 import { tableTokens } from './Tokens'
 import { tableUserMeta } from './UserMeta'
@@ -8,37 +8,34 @@ import { tableUserMeta } from './UserMeta'
 const enumUserRole = pgEnum('userRole', ROLES_ARRAY)
 const enumUserStatus = pgEnum('userStatus', STATUS_ARRAY)
 
-const tableUsers = pgTable('users', {
-  id: text('id')
-    .$defaultFn(() => createId())
-    .primaryKey(),
-  username: text('username').unique().notNull(),
-  firstName: text('first_name'),
-  lastName: text('last_name'),
-  email: text('email').unique().notNull(),
-  role: enumUserRole('role').default(ROLES.DEFAULT),
-  password: text('password'),
-  status: enumUserStatus('status').default(STATUS.UNVERIFIED),
-  createdAt: timestamp('created_at').default(sql`now()`),
-  deletedAt: timestamp('deleted_at'),
-})
+const tableUsers = pgTable(
+  'users',
+  {
+    id: text('id')
+      .$defaultFn(() => createId())
+      .primaryKey(),
+    username: text('username').unique().notNull(),
+    firstName: text('first_name'),
+    lastName: text('last_name'),
+    email: text('email').unique().notNull(),
+    role: enumUserRole('role').default(ROLES.DEFAULT),
+    password: text('password'),
+    status: enumUserStatus('status').default(STATUS.UNVERIFIED),
+    createdAt: timestamp('created_at').default(sql`now()`),
+    deletedAt: timestamp('deleted_at'),
+  },
+  (table) => ({
+    idIdx: index('username_idx').on(table.id),
+    usernameIdx: index('username_idx').on(table.username),
+    emailIdx: index('email_idx').on(table.email),
+  })
+)
 
-const viewUsers = pgView('users', {
-  id: text('id')
-    .$defaultFn(() => createId())
-    .primaryKey(),
-  username: text('username').unique().notNull(),
-  firstName: text('first_name'),
-  lastName: text('last_name'),
-  email: text('email').unique().notNull(),
-  role: enumUserRole('role').default(ROLES.DEFAULT),
-  status: enumUserStatus('status').default(STATUS.UNVERIFIED),
-  deletedAt: timestamp('deleted_at'),
-}).existing()
+// TODO: Deleted viewUsers - revisit sometime
 
 const usersRelations = relations(tableUsers, ({ one, many }) => ({
   meta: one(tableUserMeta, { fields: [tableUsers.id], references: [tableUserMeta.userId] }),
   tokens: many(tableTokens),
 }))
 
-export { enumUserRole, enumUserStatus, tableUsers, viewUsers, usersRelations }
+export { enumUserRole, enumUserStatus, tableUsers, usersRelations }
