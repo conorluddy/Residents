f43778172af361ea6a29eb3507c7926b0b1d09d8
Conor <conorluddy@gmail.com>
2024-09-22 17:32:00 +0100

    🔍 Filtering added to getAllUsers 🔍 (#156)

--- src/controllers/users/getAllUsers.ts
+++ src/controllers/users/getAllUsers.ts
@@ -2,18 +2,51 @@ import SERVICES from '../../services'
 import { Request, Response } from 'express'
 import { handleSuccessResponse } from '../../middleware/util/successHandler'
 import { DEFAULT_PAGE_SIZE_ROW_LIMIT } from '../../services/user/getAllUsers'
-import { getQueryNumber } from '../../utils/queryStrings'
+import { getQueryNumber, getStringFromQuery } from '../../utils/queryStrings'
+import { ROLES, ROLES_ARRAY, STATUS, STATUS_ARRAY } from '../../constants/database'
+import { BadRequestError } from '../../errors'
+import MESSAGES from '../../constants/messages'
 
 /**
  * getAllUsers
  */
 export const getAllUsers = async (req: Request, res: Response): Promise<Response> => {
+  // Pagination
   const limit = getQueryNumber(req.query?.limit, DEFAULT_PAGE_SIZE_ROW_LIMIT)
   const offset = getQueryNumber(req.query?.offset, 0)
 
+  // Search
+  const firstName = getStringFromQuery(req.query?.firstName)
+  const lastName = getStringFromQuery(req.query?.lastName)
+  const email = getStringFromQuery(req.query?.email)
+  const username = getStringFromQuery(req.query?.username)
+
+  // Filters
+  const role = getStringFromQuery(req.query?.role)
+  const status = getStringFromQuery(req.query?.status)
+
+  // Validate the role and status, TODO: could be moved to a middleware validator for this route
+  if (role && !ROLES_ARRAY.includes(role as ROLES)) {
+    throw new BadRequestError(MESSAGES.INVALID_ROLE)
+  }
+  if (status && !STATUS_ARRAY.includes(status as STATUS)) {
+    throw new BadRequestError(MESSAGES.INVALID_ROLE)
+  }
+  const verifiedRole = role as ROLES | undefined
+  const verifiedStatus = status as STATUS | undefined
+
   // TODO: Add a WITHMETA flag to include the user meta data
-  // TODO: Filtering / searching / pagination
-  const users = await SERVICES.getAllUsers({ offset, limit })
+
+  const users = await SERVICES.getAllUsers({
+    offset,
+    limit,
+    firstName,
+    lastName,
+    email,
+    username,
+    role: verifiedRole,
+    status: verifiedStatus,
+  })
 
   return handleSuccessResponse({ res, users })
 }
--- src/services/user/getAllUsers.ts
+++ src/services/user/getAllUsers.ts
@@ -1,27 +1,63 @@
+import db from '../../db'
 import { tableUsers } from '../../db/schema'
 import { SafeUser } from '../../db/types'
-import db from '../../db'
-import { asc } from 'drizzle-orm'
+import { asc, ilike, eq, and } from 'drizzle-orm'
+import { ROLES, STATUS } from '../../constants/database'
 
-interface PaginationProps {
+interface GetAllUsersProps {
   offset: number
   limit: number
+  firstName?: string
+  lastName?: string
+  email?: string
+  username?: string
+  role?: ROLES
+  status?: STATUS
 }
 
 // Hard limit until we have pagination
 export const DEFAULT_PAGE_SIZE_ROW_LIMIT = 100
 export const MAX_PAGE_SIZE_ROW_LIMIT = 1000
 
-const getAllUsers = async (
-  { offset, limit }: PaginationProps = { offset: 0, limit: DEFAULT_PAGE_SIZE_ROW_LIMIT }
-): Promise<SafeUser[]> => {
-  //
-  // Cap the limit - can move these limits to .env
-  const cappedLimit = Math.min(limit, MAX_PAGE_SIZE_ROW_LIMIT)
-  //
+// TODO: Look into using https://orm.drizzle.team/docs/select#conditional-select
+// to maybe filter the returned columns based on some RBAC definitions
+// TODO https://orm.drizzle.team/docs/operators#arrayoverlaps for roles and status
 
-  // TODO: Role based filtering, possibly
-  const users: SafeUser[] = await db
+/**
+ * Get all users
+ */
+const getAllUsers = async (
+  { offset, limit, firstName, lastName, email, username, role, status }: GetAllUsersProps = {
+    offset: 0,
+    limit: DEFAULT_PAGE_SIZE_ROW_LIMIT,
+  }
+): Promise<SafeUser[]> => {
+  const cappedLimit = Math.min(limit, MAX_PAGE_SIZE_ROW_LIMIT) // Cap the limit - can move these limits to .env
+
+  const conditions = []
+
+  if (firstName) {
+    conditions.push(ilike(tableUsers.firstName, `%${firstName}%`))
+  }
+  if (lastName) {
+    conditions.push(ilike(tableUsers.lastName, `%${lastName}%`))
+  }
+  if (email) {
+    conditions.push(ilike(tableUsers.email, `%${email}%`))
+  }
+  if (username) {
+    conditions.push(ilike(tableUsers.username, `%${username}%`))
+  }
+  if (role) {
+    conditions.push(eq(tableUsers.role, role))
+  }
+  if (status) {
+    conditions.push(eq(tableUsers.status, status))
+  }
+
+  // TODO: Search by fullname!
+
+  const query = db
     .select({
       id: tableUsers.id,
       username: tableUsers.username,
@@ -34,9 +70,12 @@ const getAllUsers = async (
       deletedAt: tableUsers.deletedAt,
     })
     .from(tableUsers)
+    .where(and(...conditions))
     .orderBy(asc(tableUsers.id))
     .limit(cappedLimit)
     .offset(offset)
+
+  const users: SafeUser[] = await query
 
   return users
 }
