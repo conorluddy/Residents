1055a5dbbb4909bf885abd56b348da4ea94a7247
Conor Luddy <conorluddy@gmail.com>
2024-09-26 18:57:06 +0100

    Tests for svc

--- src/services/user/createUserMeta.ts
+++ src/services/user/createUserMeta.ts
@@ -3,7 +3,10 @@ import { tableUserMeta } from '../../db/schema'
 import { Meta } from '../../db/types'
 
 /**
- * createUserMeta - Service to create a user meta record.
+ * createUserMeta
+ * Service to create a user meta record.
+ * Initially we'll just be creating a record with the userId when the user is created.
+ * Updates to this record will be made as we add more features.
  * @param {userId} userId - The userId to create meta for
  * @returns {Promise<User["id"] | null>} - The new user object.
  */
