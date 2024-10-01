9e83386ed0dd710aef6e81c4c2ac7803c843e10e
Conor Luddy <conorluddy@gmail.com>
2024-10-01 20:19:29 +0100

    Fix magicLogin tests

--- src/controllers/auth/magicLogin.test.ts
+++ src/controllers/auth/magicLogin.test.ts
@@ -1,20 +1,34 @@
+import { makeAFakeUser } from '../../test-utils/mockUsers'
 import { Response } from 'express'
-import { HTTP_SERVER_ERROR } from '../../constants/http'
-import { User } from '../../db/types'
+import { HTTP_SUCCESS } from '../../constants/http'
 import { magicLogin } from './magicLogin'
 import MESSAGES from '../../constants/messages'
 import { ResidentRequest } from '../../types'
+import { REQUEST_EMAIL } from '../../types/requestSymbols'
+import { logger } from '../../utils/logger'
+import { sendMail } from '../../mail/sendgrid'
+import SERVICES from '../../services'
 
-describe.skip('Controller: Magic Login', () => {
-  let mockRequest: Partial<ResidentRequest> & { body: Partial<User> }
+jest.mock('../../services/index', () => ({
+  getUserByEmail: jest
+    .fn()
+    .mockResolvedValueOnce(makeAFakeUser({ email: 'bananaman@ireland.ie' }))
+    .mockResolvedValueOnce(null),
+  deleteMagicTokensByUserId: jest.fn().mockReturnThis(),
+  createToken: jest.fn().mockReturnValue('123'),
+}))
+jest.mock('../../mail/sendgrid', () => ({
+  sendMail: jest.fn(),
+}))
+
+describe('Controller: Magic Login', () => {
+  let mockRequest: Partial<ResidentRequest> & { [REQUEST_EMAIL]?: string }
   let mockResponse: Partial<Response>
 
   beforeEach(() => {
+    jest.clearAllMocks()
     mockRequest = {
-      body: {
-        username: 'MrFake',
-        email: 'mrfake@gmail.com',
-      },
+      [REQUEST_EMAIL]: 'bananaman@ireland.ie',
     }
     mockResponse = {
       status: jest.fn().mockReturnThis(),
@@ -22,16 +36,28 @@ describe.skip('Controller: Magic Login', () => {
     }
   })
 
-  it('NOT IMPLEMENTED YET: TODO', async () => {
-    await magicLogin(mockRequest as ResidentRequest, mockResponse as Response)
-    expect(mockResponse.json).toHaveBeenCalledWith({ message: MESSAGES.FEATURE_NOT_IMPLEMENTED })
-    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SERVER_ERROR.NOT_IMPLEMENTED)
-  })
-
   it('should bomb out early if you dont provide an email address', async () => {
-    delete mockRequest.body.email
+    delete mockRequest[REQUEST_EMAIL]
     await expect(magicLogin(mockRequest as ResidentRequest, mockResponse as Response)).rejects.toThrow(
       MESSAGES.EMAIL_REQUIRED
     )
+  })
+
+  it('should create a token and send an email to the user if the user email is found', async () => {
+    await magicLogin(mockRequest as ResidentRequest, mockResponse as Response)
+    expect(logger.info).toHaveBeenCalledWith('Magic login email sent. bananaman@ireland.ie, token id: 123')
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: MESSAGES.CHECK_EMAIL_MAGIC_LINK })
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SUCCESS.OK)
+    expect(SERVICES.createToken).toHaveBeenCalled()
+    expect(sendMail).toHaveBeenCalled()
+  })
+
+  it('shouldnt actually create a token or send a mail if user not found, but send same response', async () => {
+    await magicLogin(mockRequest as ResidentRequest, mockResponse as Response)
+    expect(logger.info).not.toHaveBeenCalled()
+    expect(sendMail).not.toHaveBeenCalled()
+    expect(SERVICES.createToken).not.toHaveBeenCalled()
+    expect(mockResponse.json).toHaveBeenCalledWith({ message: MESSAGES.CHECK_EMAIL_MAGIC_LINK })
+    expect(mockResponse.status).toHaveBeenCalledWith(HTTP_SUCCESS.OK)
   })
 })
