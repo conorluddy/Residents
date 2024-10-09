3bdd640567265b71f246e1b23d7b1f9d23a6b47a
Conor Luddy <conorluddy@gmail.com>
2024-10-09 19:52:44 +0100

    Rough initial docs for user create and delete. Fix schema importing.

--- src/routes/users/createUser.ts
+++ src/routes/users/createUser.ts
@@ -4,6 +4,34 @@ import MW from '../../middleware'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NewUser'
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/register', MW.VALIDATE.email, CONTROLLERS.USER.createUser)
 //                      ^
 //                      RBAC.checkCanCreateUsers
