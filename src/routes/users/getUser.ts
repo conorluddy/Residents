9011dc302195c1dbb1b6ada0deca619fb6cecff1
Conor Luddy <conorluddy@gmail.com>
2024-10-10 06:38:34 +0100

    Stubbing out more Swagger docs. Schemas need updating.

--- src/routes/users/getUser.ts
+++ src/routes/users/getUser.ts
@@ -5,6 +5,24 @@ import CONTROLLERS from '../../controllers'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get User
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: User data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       Other:
+ *         description: TODO - Swagger Response Schema
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/:id', authenticateToken, RBAC.getTargetUser, RBAC.checkCanGetUser, CONTROLLERS.USER.getUser)
 
 export default router
--- src/routes/users/updateUser.ts
+++ src/routes/users/updateUser.ts
@@ -5,6 +5,34 @@ import CONTROLLERS from '../../controllers'
 
 const router = Router()
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   patch:
+ *     summary: Update User
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UpdateUser'
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Successfully updated user.
+ *       Other:
+ *         description: TODO - Swagger Response Schema
+ *       500:
+ *         description: Internal server error
+ */
 router.patch('/:id', authenticateToken, RBAC.getTargetUser, RBAC.checkCanUpdateUser, CONTROLLERS.USER.updateUser)
 
 export default router
