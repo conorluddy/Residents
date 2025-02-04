4ffb8d3e71a3304453890151a9e065b978a288bd
Conor Luddy <conorluddy@gmail.com>
2025-02-04 08:42:47 +0000

    Fix typing

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken'
 import { EXPIRATION_JWT_TOKEN, JWT_TOKEN_SECRET } from '../config'
+import { StringValue } from 'ms' // This is a jsonwebtoken dep
 import { PublicUser, SafeUser, User } from '../db/types'
 import { userToPublicUser } from './user'
 import MESSAGES from '../constants/messages'
+
+const DEFAULT_EXPIRATION = '1h'
 
 export const generateJwtFromUser = (user: User | SafeUser | PublicUser, expiryOverride?: string): string => {
   if (!JWT_TOKEN_SECRET) {
     throw new Error(MESSAGES.JWT_SECRET_NOT_FOUND)
   }
-  return jwt.sign(userToPublicUser(user), JWT_TOKEN_SECRET, { expiresIn: expiryOverride ?? EXPIRATION_JWT_TOKEN })
+  return jwt.sign(userToPublicUser(user), JWT_TOKEN_SECRET, {
+    expiresIn: (expiryOverride ?? EXPIRATION_JWT_TOKEN ?? DEFAULT_EXPIRATION) as StringValue,
+  })
 }
